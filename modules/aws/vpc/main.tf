9e91c91b264182e30041d59590a29705985671ed
kobayashi-m42 <m42kobayashi@gmail.com>
2018-12-09 14:18:17 +0900

    :sparkles: #16 NetworkACLを構築

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -146,3 +146,211 @@ resource "aws_route_table_association" "private_1d" {
   route_table_id = "${aws_route_table.private.id}"
   subnet_id      = "${aws_subnet.private_1d.id}"
 }
+
+resource "aws_network_acl" "public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    action     = "allow"
+    from_port  = 80
+    protocol   = "tcp"
+    rule_no    = 100
+    to_port    = 80
+    cidr_block = "0.0.0.0/0"
+  }
+
+  ingress {
+    action     = "allow"
+    from_port  = 443
+    protocol   = "tcp"
+    rule_no    = 110
+    to_port    = 443
+    cidr_block = "0.0.0.0/0"
+  }
+
+  ingress {
+    action     = "allow"
+    from_port  = 22
+    protocol   = "tcp"
+    rule_no    = 120
+    to_port    = 22
+    cidr_block = "0.0.0.0/0"
+  }
+
+  ingress {
+    action     = "allow"
+    from_port  = 3389
+    protocol   = "tcp"
+    rule_no    = 130
+    to_port    = 3389
+    cidr_block = "${aws_vpc.vpc.cidr_block}"
+  }
+
+  ingress {
+    action     = "allow"
+    from_port  = 1024
+    protocol   = "tcp"
+    rule_no    = 140
+    to_port    = 65535
+    cidr_block = "0.0.0.0/0"
+  }
+
+  egress {
+    action     = "allow"
+    from_port  = 80
+    protocol   = "tcp"
+    rule_no    = 100
+    to_port    = 80
+    cidr_block = "0.0.0.0/0"
+  }
+
+  egress {
+    action     = "allow"
+    from_port  = 443
+    protocol   = "tcp"
+    rule_no    = 110
+    to_port    = 443
+    cidr_block = "0.0.0.0/0"
+  }
+
+  egress {
+    action     = "allow"
+    from_port  = 3306
+    protocol   = "tcp"
+    rule_no    = 120
+    to_port    = 3306
+    cidr_block = "${aws_vpc.vpc.cidr_block}"
+  }
+
+  egress {
+    action     = "allow"
+    from_port  = 1024
+    protocol   = "tcp"
+    rule_no    = 130
+    to_port    = 65535
+    cidr_block = "0.0.0.0/0"
+  }
+
+  egress {
+    action     = "allow"
+    from_port  = 22
+    protocol   = "tcp"
+    rule_no    = 140
+    to_port    = 22
+    cidr_block = "0.0.0.0/0"
+  }
+
+  subnet_ids = [
+    "${aws_subnet.public_1a.id}",
+    "${aws_subnet.public_1c.id}",
+    "${aws_subnet.public_1d.id}",
+  ]
+
+  tags {
+    Name = "${terraform.workspace}_public"
+  }
+}
+
+resource "aws_network_acl" "private" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    action     = "allow"
+    from_port  = 80
+    protocol   = "tcp"
+    rule_no    = 100
+    to_port    = 80
+    cidr_block = "${aws_vpc.vpc.cidr_block}"
+  }
+
+  ingress {
+    action     = "allow"
+    from_port  = 443
+    protocol   = "tcp"
+    rule_no    = 110
+    to_port    = 443
+    cidr_block = "${aws_vpc.vpc.cidr_block}"
+  }
+
+  ingress {
+    action     = "allow"
+    from_port  = 22
+    protocol   = "tcp"
+    rule_no    = 120
+    to_port    = 22
+    cidr_block = "${aws_vpc.vpc.cidr_block}"
+  }
+
+  ingress {
+    action     = "allow"
+    from_port  = 3306
+    protocol   = "tcp"
+    rule_no    = 130
+    to_port    = 3306
+    cidr_block = "${aws_vpc.vpc.cidr_block}"
+  }
+
+  ingress {
+    action     = "allow"
+    from_port  = 3389
+    protocol   = "tcp"
+    rule_no    = 140
+    to_port    = 3389
+    cidr_block = "${aws_vpc.vpc.cidr_block}"
+  }
+
+  ingress {
+    action     = "allow"
+    from_port  = 1024
+    protocol   = "tcp"
+    rule_no    = 150
+    to_port    = 65535
+    cidr_block = "0.0.0.0/0"
+  }
+
+  egress {
+    action     = "allow"
+    from_port  = 80
+    protocol   = "tcp"
+    rule_no    = 100
+    to_port    = 80
+    cidr_block = "0.0.0.0/0"
+  }
+
+  egress {
+    action     = "allow"
+    from_port  = 443
+    protocol   = "tcp"
+    rule_no    = 110
+    to_port    = 443
+    cidr_block = "0.0.0.0/0"
+  }
+
+  egress {
+    action     = "allow"
+    from_port  = 1024
+    protocol   = "tcp"
+    rule_no    = 120
+    to_port    = 65535
+    cidr_block = "0.0.0.0/0"
+  }
+
+  egress {
+    action     = "allow"
+    from_port  = 22
+    protocol   = "tcp"
+    rule_no    = 130
+    to_port    = 22
+    cidr_block = "0.0.0.0/0"
+  }
+
+  subnet_ids = [
+    "${aws_subnet.private_1a.id}",
+    "${aws_subnet.private_1c.id}",
+    "${aws_subnet.private_1d.id}",
+  ]
+
+  tags {
+    Name = "${terraform.workspace}_private"
+  }
+}
