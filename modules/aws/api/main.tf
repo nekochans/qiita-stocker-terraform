6e75253d9d296d177da89c5b58b142d9811df94c
keitakn <keita.koga.work@gmail.com>
2019-01-10 23:41:58 +0900

    :recycle: #21 Route53のリソースを別ファイルに分離

--- modules/aws/api/main.tf
+++ modules/aws/api/main.tf
@@ -35,19 +35,3 @@ resource "aws_security_group" "api" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-data "aws_route53_zone" "api" {
-  name = "${var.main_domain_name}"
-}
-
-resource "aws_route53_record" "api" {
-  zone_id = "${data.aws_route53_zone.api.zone_id}"
-  name    = "${lookup(var.sub_domain_name, "${terraform.env}.name", var.sub_domain_name["default.name"])}"
-  type    = "A"
-
-  alias {
-    name                   = "${aws_lb.api.dns_name}"
-    zone_id                = "${aws_lb.api.zone_id}"
-    evaluate_target_health = false
-  }
-}
