eb3cfb6fe337152b7d9ca637f38466cd3470f079
keitakn <keita.koga.work@gmail.com>
2019-01-10 23:43:53 +0900

    :recycle: #21 EC2のセキュリティグループをec2.tfに移動

--- modules/aws/api/ec2.tf
+++ modules/aws/api/ec2.tf
@@ -1,3 +1,41 @@
+resource "aws_security_group" "api" {
+  name        = "${terraform.workspace}-${lookup(var.api, "${terraform.env}.name", var.api["default.name"])}"
+  description = "Security Group to ${lookup(var.api, "${terraform.env}.name", var.api["default.name"])}"
+  vpc_id      = "${lookup(var.vpc, "vpc_id")}"
+
+  tags {
+    Name = "${terraform.workspace}-${lookup(var.api, "${terraform.env}.name", var.api["default.name"])}"
+  }
+
+  ingress {
+    from_port       = 80
+    protocol        = "tcp"
+    to_port         = 80
+    security_groups = ["${aws_security_group.alb.id}"]
+  }
+
+  ingress {
+    from_port       = 80
+    protocol        = "tcp"
+    to_port         = 80
+    security_groups = ["${lookup(var.bastion, "bastion_security_id")}"]
+  }
+
+  ingress {
+    from_port       = 22
+    protocol        = "tcp"
+    to_port         = 22
+    security_groups = ["${lookup(var.bastion, "bastion_security_id")}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 data "aws_iam_policy_document" "ec2_trust_relationship" {
   "statement" {
     effect = "Allow"
