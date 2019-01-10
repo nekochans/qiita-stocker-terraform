40dc52e12d1cb5b3e59c081a56a2081741e08a0d
keitakn <keita.koga.work@gmail.com>
2019-01-10 23:32:59 +0900

    :wrench: #21 ディスク容量が8GBだと少し不安なので無料枠ギリギリの30GBに変更

--- modules/aws/api/variable.tf
+++ modules/aws/api/variable.tf
@@ -7,7 +7,7 @@ variable "api" {
     default.ami           = "ami-00f9d04b3b3092052"
     default.instance_type = "t2.micro"
     default.volume_type   = "gp2"
-    default.volume_size   = "8"
+    default.volume_size   = "30"
   }
 }
 
