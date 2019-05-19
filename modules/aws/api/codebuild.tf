e0bbba1f9b0514f1359fed8bb3bb719189502725
keitakn <keita.koga.work@gmail.com>
2019-05-19 14:02:50 +0900

    :sparkles: #82 ECRにプッシュを行うCodeBuildプロジェクトを作成

--- modules/aws/api/codebuild.tf
+++ modules/aws/api/codebuild.tf
@@ -183,3 +183,57 @@ resource "aws_codebuild_project" "api_rds_migration" {
     vpc_id = "${lookup(var.vpc, "vpc_id")}"
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_codebuild_project" "push_to_ecr" {
+  "artifacts" {
+    type = "NO_ARTIFACTS"
+  }
+
+  "environment" {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/docker:18.09.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    environment_variable {
+      name  = "DEPLOY_STAGE"
+      value = "${terraform.workspace}"
+    }
+
+    environment_variable {
+      name  = "AWS_ACCOUNT_ID"
+      value = "${data.aws_caller_identity.current.account_id}"
+    }
+
+    environment_variable {
+      name  = "IMAGE_TAG"
+      value = "latest"
+    }
+
+    environment_variable {
+      name  = "REPOSITORY_NGINX"
+      value = "${terraform.workspace}-api-nginx"
+    }
+
+    environment_variable {
+      name  = "REPOSITORY_PHP"
+      value = "${terraform.workspace}-api-php"
+    }
+  }
+
+  name         = "${lookup(var.fargate, "${terraform.env}.name", var.fargate["default.name"])}-push-ecr"
+  service_role = "${aws_iam_role.codebuild_role.arn}"
+
+  "source" {
+    type                = "GITHUB"
+    location            = "https://github.com/nekochans/qiita-stocker-backend.git"
+    git_clone_depth     = 1
+    buildspec           = "buildspec-push-ecr.yml"
+    report_build_status = true
+  }
+
+  badge_enabled = true
+}
