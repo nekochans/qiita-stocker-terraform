a2b4b71de625f89a63e276771652823154015e14
kobayashi-m42 <m42kobayashi@gmail.com>
2019-08-04 11:48:50 +0900

    :sparkles: #111 nuxt配信用のCloudFrontとS3バケットを追加

--- modules/aws/frontend/variable.tf
+++ modules/aws/frontend/variable.tf
@@ -4,6 +4,19 @@ variable "bucket" {
   default = "qiita-stocker-frontend"
 }
 
+variable "bucket_nuxt" {
+  type = string
+
+  default = "qiita-stocker-nuxt"
+}
+
+variable "api_gateway" {
+  type = string
+
+  default = "qiita-stocker-nuxt"
+}
+
+
 variable "main_domain_name" {
   type = string
 
@@ -14,8 +27,9 @@ variable "sub_domain_name" {
   type = map(string)
 
   default = {
-    "stg.name"     = "stg-www"
+    "stg.name"     = "tmp-stg-www"
     "default.name" = "www"
+    "stg.tmp_name" = "stg-www"
   }
 }
 
